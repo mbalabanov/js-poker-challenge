7dc4786f880880e9e66395f2f82abc62b858d0d1
mbalabanov <marincomics@gmail.com>
2022-07-24 16:05:52 +0300

    Solves initial poker pair exercise

--- src/poker.js
+++ src/poker.js
@@ -1,4 +1,35 @@
-function winningPair() {}
+const CARD_VALUES = {
+  A: 14,
+  K: 13,
+  Q: 12,
+  J: 11,
+  10: 10,
+  9: 9,
+  8: 8,
+  7: 7,
+  6: 6,
+  5: 5,
+  4: 4,
+  3: 3,
+  2: 2
+}
+
+function winningPair(cardPair1, cardPair2) {
+  const pairValues1 = addPairValues(cardPair1)
+  const pairValues2 = addPairValues(cardPair2)
+
+  return pairValues1 > pairValues2 ? cardPair1 : cardPair2
+}
+
+function addPairValues(cardPair) {
+  let totalValueOfPair = 0
+
+  for (let card of cardPair) {
+    totalValueOfPair += CARD_VALUES[card]
+  }
+
+  return totalValueOfPair
+}
 
 // Extension criteria
 
